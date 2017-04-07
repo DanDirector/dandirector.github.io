17639f8ee63f433edff99446550f9ca5eae38973
DanDirector <Dandirector@mail.ru>
2017-04-07 19:46:36 +0200

    Update main.js

--- mobile/main.js
+++ mobile/main.js
@@ -1,183 +1,76 @@
-$(document).ready(function() {
+'use strict';
 
-  var $demo = $('.demo');
-  var $items = $('.demo__items');
-  var $line = $('.demo__line');
-  var $printer = $('.printer');
-  var $printerItemCont = $('.printer__item-cont');
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) {if (window.CP.shouldStopExecution(2)){break;} var source = arguments[i]; for (var key in source) {if (window.CP.shouldStopExecution(1)){break;} if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } }
+window.CP.exitedLoop(1);
+ }
+window.CP.exitedLoop(2);
+ return target; };
 
-  var maxDragY = 150;
-  var minDragY = 60;
-  var printerInitY = -90;
-  var dragResistance = 0.6;
-  var deltaY = 0;
-  var printing = false;
+var app = document.getElementById('app');
+var hearts = document.querySelectorAll('.fa-heart');
 
-  var printStep1AT = 0.3;
-  var printStep2Delay = printStep1AT + 0.1;
-  var printStep2AT = 1.8;
-  var printStep3Delay = printStep2Delay + printStep2AT;
-  var printStep3AT = 0.55;
-  var printFullAT = printStep3Delay + printStep3AT;
+var hApp = new Hammer(app, {
+  direction: Hammer.DIRECTION_ALL,
+  threshold: 0
+});
 
-  var itemsInfo = [
-    {
-      heading: 'My Twitter',
-      amount: '@NikolayTalanov',
-      amountLink: 'https://twitter.com/NikolayTalanov',
-      info: 'Follow me on Twitter!',
-      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/twitter big icon.png',
-      details: 'How is your day?',
-      when: 'Just now',
-      imgBackColor: '#1DA1F2'
-    },
-    {
-      heading: 'Dribbble Shot',
-      amount: 'Shot by Saptarshi Prakash',
-      amountLink: 'https://dribbble.com/shots/3031884-Pull-to-Refresh-Printer',
-      info: 'Thanks to this guy for cool gif!',
-      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/dribble big icon.png',
-      details: 'Hi!',
-      when: 'It\'s been a while',
-      imgBackColor: '#ea4c89'
-    },
-    {
-      heading: 'Other demos',
-      amount: 'My codepen',
-      amountLink: 'http://codepen.io/suez/',
-      info: 'Check out my other demos',
-      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/codepen big icon.png',
-      details: 'Hola',
-      when: '666 min',
-      imgBackColor: '#000'
-    }
-  ];
-  
-  var itemCounter = 0;
-  var $itemBoilerplate = $('.item--boilerplate');
+hApp.get('pan').set({ direction: Hammer.DIRECTION_ALL });
 
-  function generateNewItem() {
-    var $item = $itemBoilerplate.clone();
-    var $itemFront = $('.item__front', $item);
-    var data = itemsInfo[itemCounter];
-    itemCounter++;
-    if (itemCounter > 2) itemCounter = 0;
-    
-    $item.removeClass('item--boilerplate');
-    
-    Object.keys(data).forEach(function(prop) {
-      var $el = $('.item__' + prop, $itemFront);
-      var val = data[prop];
-      
-      if (prop === 'imgBackColor') return;
-      if (prop === 'img') {
-        $el.attr('src', val);
-      } else if (prop === 'amountLink') {
-        $el.attr('href', val);
-      } else {
-        $el.text(val);
-      }
-    });
-    
-    $('.item__back .item__img', $item).css('background', data.imgBackColor);
-    
-    return $item;
-  };
+var pan$ = Rx.Observable.fromEventPattern(function (cb) {
+  return hApp.on('panstart panend panup pandown', cb);
+});
 
-  function insertNewItem() {
-    var $newItem = generateNewItem();
+var initialState = {
+  item: 0,
+  y: 0,
+  moving: false
+};
 
-    $printerItemCont.empty();
-    $printerItemCont.append($newItem);
-  };
-  
-  insertNewItem();
+var reducer = function reducer() {
+  var state = arguments.length <= 0 || arguments[0] === undefined ? initialState : arguments[0];
+  var action = arguments[1];
 
-  function cloneAndPlacePrintedItem() {
-    var $printedItem = $('.item--in-printer');
-    var $clone = $printedItem.clone();
-    var itemOffsets = $printedItem.offset();
-    var demoOffsets = $demo.offset();
-    var left = itemOffsets.left - demoOffsets.left;
-    var top = itemOffsets.top - demoOffsets.top;
+  switch (action.type) {
+    case 'panup':
+    case 'pandown':
+      return _extends({}, state, {
+        y: action.deltaY,
+        moving: true
+      });
 
-    $clone.removeClass('item--in-printer').addClass('s--cloned');
-    $clone.css({left: left, top: top});
+    case 'panend':
+      {
+        var newItem = action.deltaY > 100 ? Math.max(state.item - 1, 0) : action.deltaY < 100 ? Math.min(state.item + 1, 2) : state.item;
 
-    $demo.append($clone);
-
-    setTimeout(function() {
-      var $finalClone = $clone.clone();
-      $finalClone.attr('style', '');
-      $finalClone.removeClass('s--cloned');
-
-      $items.prepend($finalClone);
-      insertNewItem();
-      
-      setTimeout(function() {
-        $clone.remove();
-      }, 50);
-    }, printStep3AT * 1000);
-  };
-
-  function runPrintAnimation() {
-    $demo.addClass('s--printing');
-
-    setTimeout(function() {
-      cloneAndPlacePrintedItem();
-      $demo.css('top');
-      $demo.addClass('s--printing-step-3');
-    }, printStep3Delay * 1000);
-  };
-
-  $(document).on('mousedown touchstart', '.demo__items', function(e) {
-    if (printing) return;
-    console.log('click touch');
-    var startY = e.pageY || e.originalEvent.touches[0].pageY;
-
-    $items.addClass('s--no-select');
-
-    $(document).on('mousemove touchmove', function(e) {
-      e.preventDefault();
-      var y = e.pageY || e.originalEvent.touches[0].pageY;
-      deltaY = (y - startY) * dragResistance;
-
-      if (deltaY < 0) deltaY = 0;
-      if (deltaY > maxDragY) deltaY = maxDragY;
-
-      var progress = deltaY / maxDragY;
-      var printerY = printerInitY * progress * -1;
-
-      $items.css('transform', 'translate3d(0,'+ deltaY +'px,0)');
-      $line.css('transform', 'scaleX('+ Math.sqrt(progress) +')');
-      $printer.css('transform', 'translate3d(0,'+ printerY +'px,0)');
-    });
-
-    $(document).on('mouseup touchend', function() {
-      $(document).off('mousemove touchmove mouseup touchend');
-      $items.removeClass('s--no-select');
-      if (!deltaY) return;
-
-      printing = true;
-      var _printAT = printFullAT;
-
-      if (deltaY >= minDragY) {
-        runPrintAnimation();
-      } else {
-        $demo.addClass('s--reset');
-        _printAT = printStep1AT;
+        return {
+          y: 0,
+          moving: false,
+          item: newItem
+        };
       }
 
-      setTimeout(function() {
-        $demo.removeClass('s--printing s--printing-step-3 s--reset');
-        $items.attr('style', '');
-        $line.attr('style', '');
-        $printer.attr('style', '');
+    default:
+      return state;
+  }
+};
 
-        printing = false;
-        deltaY = 0;
-      }, _printAT * 1000);
+var state$ = pan$.scan(reducer, initialState);
+
+var style$ = RxCSS({ pan: state$ });
+
+// Since you've scrolled this far down,
+// here's an easter egg!
+
+var colorPairs = [['#F82462', '#1D2063'], ['#24B8F8', '#24315A'], ['#FF5C43', '#343AA6'], ['#3EBB8C', '#FFD8C0']];
+
+hearts.forEach(function (heart, i) {
+  var _colorPairs$i = colorPairs[i];
+  var primary = _colorPairs$i[0];
+  var secondary = _colorPairs$i[1];
+
+  heart.addEventListener('click', function () {
+    RxCSS.styledash(document.documentElement).set({
+      color: { primary: primary, secondary: secondary }
     });
   });
-
 });
