a219e46f802731b9d248476aa8115e678a2d705a
dan <dandirector47@gmail.com>
2018-03-15 19:41:30 +0200

    big commit

---
+++ rncompany/js/custom.js
@@ -0,0 +1,42 @@
+
+jQuery(document).ready(function() {
+finalDate    
+	/*
+		Preloader
+	*/
+	
+  	$(window).load(function() {
+
+   	// will first fade out the loading animation 
+    	$("#status").fadeOut("slow"); 
+
+    	// will fade out the whole DIV that covers the website. 
+    	$("#preloader").delay(500).fadeOut("slow").remove();      
+
+  	}) 
+
+	/*
+		Final Countdown Settings
+	*/
+  var finalDate = new Date();
+  finalDate.setDate(finalDate.getDate() + 1);
+  var curDate = getDate(localStorage.getItem('FD'));
+
+  function getDate(obj) {
+    return obj ? new Date(JSON.parse(obj).fd) : '';
+  };
+
+  finalDate = (curDate && curDate > new Date()) ? curDate : finalDate;
+
+	$('div.counter').countdown(finalDate)
+   	.on('update.countdown', function(event) {
+      localStorage.setItem('FD', JSON.stringify({'fd': finalDate}));
+   		$(this).html(event.strftime('<div class="days-wrapper"><span class="days">%D</span><br>дней</div>' + 
+   										 	 '<div class="hours-wrapper"><span class="hours">%H</span><br>часов</div>' + 
+   										 	 '<div class="minutes-wrapper"><span class="minutes">%M</span><br>минут</div>' +
+   										 	 '<div class="seconds-wrapper"><span class="seconds">%S</span><br>секунд</div>'));
+
+   });  
+
+});
+
--- viva/js/main.js
+++ viva/js/main.js
@@ -1,5 +1,26 @@
-$('#toggle').click(function() {
-   // $(this).toggleClass('active');
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1800:{
+            items:5
+        }
+    }})
+
+  $('#toggle').click(function() {
+   $(this).toggleClass('active');
    // $('.menu').toggleClass('open');
    $('.menu').toggle();
   });
+
+});
+
+
